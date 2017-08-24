47de19f0b256b924cbb212cfe7ad8eaa1fb7b86e
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-08-24 16:33:54 +0200

    fixed script

--- workload.sh
+++ workload.sh
@@ -3,6 +3,6 @@
 BINDIR=$(cd "$(dirname "$0")"; pwd)/
 CALL="target/org.iobserve.workloadgeneration-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 
-java -jar "$BINDIR/$CALL" -u http://172.17.0.2:8080/jpetstore -i 20 -s /home/reiner/temp/ -p /home/reiner/node_modules/phantomjs/lib/phantom/bin/phantomjs
+java -jar "$BINDIR/$CALL" $@
 
 # end
